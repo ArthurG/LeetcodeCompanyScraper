865a97d42d4b0264874a16df75f040fbaecd99cd
ArthurG <arthur.guo@uwaterloo.ca>
2020-04-08 01:03:22 -0400

    Make script fully automated
    Take over #2

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-Feel free to message me with questions. Last used and tested on Aug 31, 2018 to scrape all questions by company from Leetcode.
+## To install dependencies:
+`pip3 install -r requirements.txt`
+
+## To run:
+`python3 scrapy.py`
+
+Note: You will have to fill out your username and password in the script.
+
+Feel free to message me with questions. Last used and tested on Apr 8, 2020 to scrape all questions by company from Leetcode.
--- scrape.py
+++ scrape.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
+import os
 
 from selenium.webdriver import FirefoxOptions
 
@@ -12,6 +13,9 @@ import requests
 periods = ["6months", "1year", "2year", "alltime"]
 
 def main():
+    if not os.path.exists("scraped"):
+        os.makedirs("scraped")
+
     login_url = "https://leetcode.com/accounts/login/"
 
     chrome_opts = webdriver.ChromeOptions()
@@ -25,24 +29,59 @@ def main():
     companies = text_file.read().split('\n')
     text_file.close()
 
-    driver = webdriver.Chrome(executable_path=r'/home/arthur/Programming/LeetcodePremiumScraper/chromedriver', chrome_options=opts)
+    driver = webdriver.Chrome(executable_path=r'./chromedriver', chrome_options=opts)
     driver.maximize_window()
     driver.get(login_url)
 
-    driver.find_element_by_id("id_login").send_keys("")
-    driver.find_element_by_id("id_password").send_keys("")
-    driver.find_element_by_id("id_password").submit()
-    time.sleep(4)
+    username = input("Enter username/email: ")
+    passwd = input("Enter password: ")
 
+    time.sleep(5)
+
+    while True:
+        try:
+            driver.find_element_by_id("id_login").send_keys(username)
+            driver.find_element_by_id("id_password").send_keys(passwd)
+            driver.find_element_by_id("signin_btn").click();
+            break;
+        except:
+            time.sleep(1)
+
+    time.sleep(5)
 
     for company in companies:
         lc_url = "https://leetcode.com/company/" + company
         driver.get(lc_url)
-        #driver.find_element_by_class_name("reactable-th-frequency").click()
 
-        periods = ["6months", "1year", "2year", "alltime"]
-        for period in periods:
-            i = input("Please select {}".format(period))
+        periodNames = ['6months', '1year', '2year', 'alltime']
+        periodSelectors = ["react-select-2--option-0", "react-select-2--option-1", "react-select-2--option-2", "react-select-2--option-3" ]
+        for period_selector, period_name in zip(periodSelectors, periodNames):
+            while True:
+                try:
+                    driver.find_element_by_class_name('Select-control').click();
+                    break
+                except:
+                    print("Dropdown menu not found. Retrying.")
+                    # quick hack to make the question bubble go away
+                    try:
+                        driver.find_element_by_id('app').click();
+                        time.sleep(1)
+                    except:
+                        break
+
+            try:
+                time.sleep(0.05)
+                driver.find_element_by_id(period_selector).click()
+            except:
+                try:
+                     print("Dropdown menu item not found. Continuing.")
+                     # quick hack to make the dropdown go away
+                     driver.find_element_by_id('app').click();
+                     continue
+                except:
+                    continue
+
+            time.sleep(0.05)
 
             ans = ""
 
@@ -58,13 +97,17 @@ def main():
                     title = data[2].text
                     acceptance = data[3].text
                     difficulty = data[4].text
-                    frequency = data[5]['value']
+                    if data[5].get("value"):
+                        frequency = data[5]['value']
+                    else:
+                        frequency = 'N/A'
                     ans += (number+ "," + title+ ","+ acceptance+ ","+ difficulty+ "," +frequency+ "\n")
 
-            f = open(company + "_" + period, "w")
+            f = open("./scraped/" + company + "_" + period_name, "w")
             f.write(ans)
             f.close()
 
 
 if __name__ == "__main__":
     main()
+
